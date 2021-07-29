3e272559b8fca27c2206bcd7cd5b4b2e8cfcebd9
alka <alkar1846@mail.com>
2021-07-29 17:06:54 +0530

    title and logo change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navb from './Components/Navb';
@@ -8,10 +8,12 @@ import Projects from './Components/Projects';
 import Little_more from './Components/Little_more';
 import Education from './Components/Education';
 import Footer from './Components/Footer';
+import Title from './Components/Title';
 
 function App() {
   return (
     <div className="App">
+      <Title/>
       <Navb />
       <About/>
       <Work/>
---
+++ src/Components/Title.jsx
@@ -0,0 +1,14 @@
+import {React,useEffect} from 'react'
+
+function Title() {
+    useEffect(() => {
+      document.title="Md Arshad Ali"
+    },[])
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Title
